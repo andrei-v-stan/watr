cdea64b3aa30da8551de7b7caa1fb733f85debb0
Stan Andrei <andreistan9@gmail.com>
2025-01-31 03:09:55 +0200

    Update v0.10.1 - CSS Validation & Animations

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,29 @@
+import { useEffect, useState } from 'react';
 import '../styles/about.css';
 
 export default function About() {
+  const [firstVisit, setFirstVisit] = useState(false);
 
+  useEffect(() => {
+    if (!localStorage.getItem("visited")) {
+      setFirstVisit(true);
+      localStorage.setItem("visited", "true");
+    }
+  }, []);
 
   return (
-    <div id="about-section">
-      <img src="/src/assets/logo_board.png" alt="Logo Board" id="logo-board"></img>
-      <div id="about-content">
-        <h2>About Watr</h2>
+    <div id="about-section" className={firstVisit ? "animate" : ""}>
+      <img src="/src/assets/logo_board.png" alt="Logo Board" id="logo-board" className={firstVisit ? "animate" : ""}></img>
+      <div id="about-content" className={firstVisit ? "animate" : ""}>
+        <div id="about-first-line">
+            <h2>About Watr</h2>
+            <a href="http://jigsaw.w3.org/css-validator/check/referer" id='css-validator'>
+              <img
+                id='css-validator-image'
+                src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+                alt="Valid CSS!" />
+            </a>
+          </div>
         <h3>Project proposal (<i>by <a href="https://profs.info.uaic.ro/sabin.buraga/">Prof. Sabin-Corneliu Buraga</a></i>) </h3>
         <blockquote cite="http://www.worldwildlife.org/who/index.html">
         Create a (micro-)service-based Web system able to efficiently perform various processing tasks regarding the meta-data available in RDFa and HTML5 microdata formats provided by the <a href="http://webdatacommons.org/" title="WDC RDFa, Microdata, Embedded JSON-LD, and Microformats Data Sets">Web Data Commons</a> – see also <a href="https://cdn.aaai.org/ojs/17816/17816-13-21310-1-2-20210518.pdf" title="Read the article (PDF)">Empirical Best Practices On Using Product-Specific Schema.org</a>. Using a modular approach, a minimal set of operations will be implemented: visualize, classify, compare, and match/align. The queries will be performed by invoking a SPARQL endpoint, the obtained results being available in HTML and JSON-LD formats. Various statistics, modeled with the <a href="https://www.w3.org/TR/vocab-data-cube/" title="Details about RDF Data Cube">RDF Data Cube</a> vocabulary, will be also exposed.
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,9 @@ import '../styles/header.css';
 export default function Header() {
   return (
     <header className="app-header">
-      <h2>Watr: Web Data Commons Analyzer</h2>
+      <a href="/">
+        <h2>Web Data Commons Analyzer</h2>
+      </a>
     </header>
   );
 }
